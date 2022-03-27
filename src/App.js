1dcb2e99a8164645002c5973b831c83266028a1e
bodzash <98101027+bodzash@users.noreply.github.com>
2022-03-27 23:25:00 +0200

    build

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react"
 
 //COMPONENTS
 import Unit from "./Components/Unit"
-import Grid from "./Components/Grid"
-import Board from "./Components/board.js"
+import Battle from "./Components/Battle"
+import Command from "./Components/Command"
 
 //Data
-import igDataUnits from "./data/DataUnitsIG"
-import igDataWeapons from "./data/DataWeaponsIG"
+import DATA from "./data/DataSheetAll"
 
-import igList from "./data/DataListIG"
+//Deck/list
+import igList from "./data/ListIG"
+import orkList from "./data/ListOrk"
 
 //CSS
 import './Styles/style.css'
@@ -17,66 +18,74 @@ import './Styles/style.css'
 export default function App() {
 
   const [menuState, setMenuState] = React.useState("title") //title, lobby, game
+  const [menuSelect, setMenuSelect] = React.useState(false)
   const [selfDeck, setSelfDeck] = React.useState([])
-  const [units, setUnits] = React.useState([]) //objects with vars
-  const [grids, setGrids] = React.useState([]) //Board
-  const [board, setBoard] = React.useState({x: 14, y:9}) //y:7
+  const [otherDeck, setOtherDeck] = React.useState([])
+  const [units, setUnits] = React.useState([])
+  const [board, setBoard] = React.useState({x: 14, y:7}) //y:7
   const [selectedUnit, setSelectedUnit] = React.useState()
 
   const [myTurn, setMyTurn] = React.useState(true) //bool
-  const [phase, setPhase] = React.useState("move") //move,shoot,charge,fight
+  const [phase, setPhase] = React.useState("movement") //movement,shooting,charge,fight
+  const [subPhase, setSubPhase] = React.useState("none") //none, hit, wound, save
 
-  //console.log(selfDeck)
+  const [targetedUnit, setTargetedUnit] = React.useState(undefined)
+  const [throws, setThrows] = React.useState([]) //all throws
+  const [succThrows, setSuccThrows] = React.useState([]) //succesful throws
+  const [throwDone, setThrowDone] = React.useState(0) // 0-canClickBtn 1-continue 2-exit
+
+  React.useEffect(()=> {
+    setUnits([...selfDeck, ...otherDeck])
+  }, [selfDeck, otherDeck])
 
   //Draw all Units
   function renderUnits() {
-    return selfDeck.map((i,index) => {
+    return units.map((i,index) => {
       return <Unit
         key={index}
         id={index}
         board={board}
         turn={myTurn}
         phase={phase}
-        deck={selfDeck}
-        setDeck={setSelfDeck}
+        subPhase={subPhase}
+        setSubPhase={setSubPhase}
+        deck={units}
+        setDeck={setUnits}
         selected={selectedUnit}
         setSelected={setSelectedUnit}
+        setTargetedUnit={setTargetedUnit}
+        setThrows={setThrows}
         {...i} />
     })
   }
-  //Draw C&C window
-  function renderCommand() {
 
-    function handler() {
-      if (myTurn)
-        if (phase === "move")
-          setPhase("shoot")
-        else if (phase === "shoot")
-          setPhase("charge")
-        else if (phase === "charge")
-          setPhase("fight")
-        else if (phase === "fight")
-          setMyTurn(false)
-      setSelectedUnit(undefined)
-    }
-
-    return (
-      <div className="cnc">
-        {(myTurn && phase !== "fight") && <button onClick={handler}>
-          END <span>{phase}</span> PHASE
-        </button>}
-        {(myTurn && phase === "fight") && <button onClick={handler}>
-          END TURN
-        </button>}
-        {!myTurn && <button style={{background: "gray"}}>
-          OPPONENT'S TURN
-        </button>}
-      </div>
-    )
+  //Parse the List/Deck
+  function parseList(deckArg,isHostile) {
+    let DECK_PARSED = deckArg.map((item,index)=> {
+      let parsedUnit = DATA.find(unit=> unit.name === item)
+      return {
+        ...parsedUnit,
+        x: !isHostile ? 0 : 14,
+        y: index,
+        isEnemy: isHostile,
+        hasMoved: false,
+        hasShot: false,
+        hasCharged: false,
+        hasMeleed: false
+      }
+    })
+    if(!isHostile) {setSelfDeck(DECK_PARSED)} else {setOtherDeck(DECK_PARSED)}
   }
 
-  function renderCombatWindow() {
+  //Menu Component (local)
+  function Menu() {
+    return (<div></div>)
+  }
 
+  function handleClick(list,enemy) {
+    parseList(list,enemy)
+    menuSelect && setMenuState("game")
+    setMenuSelect(true)
   }
 
   return (
@@ -84,21 +93,29 @@ export default function App() {
       <div className="menu">
             {menuState === "title" && 
             <>
-                <h1>Select List:</h1>
+                <h1 style={{color: !menuSelect ? "blue" : "red"}}>{!menuSelect ? "Blue Player" : "Red Player" }!</h1>
+                <h1>Select An Army...</h1>
                 <div className="list-wrapper">
-                    <button onClick={()=>{setSelfDeck(igList); setMenuState("game")}}>IG</button>
+                    <button onClick={()=> handleClick(igList, menuSelect)}>Guards</button>
+                    <button onClick={()=> handleClick(orkList, menuSelect)}>Orcs</button>
                 </div>
-            </>
-            }
-            {menuState === "lobby" && 
-            <>
-                <h1>Waiting for opponent...</h1>
             </>
             }
             {menuState === "game" && 
             <div className="game">
                 {renderUnits()}
-                {renderCommand()}
+                <Command turn={myTurn} setTurn={setMyTurn} phase={phase} setPhase={setPhase} subPhase={subPhase} setSelected={setSelectedUnit} setDeck={setUnits} />
+                {subPhase !== "none" &&
+                <Battle
+                  phase={phase}
+                  throws={throws} setThrows={setThrows}
+                  deck={units} setDeck={setUnits}
+                  subPhase={subPhase} setSubPhase={setSubPhase}
+                  selected={selectedUnit} setSelected={setSelectedUnit}
+                  targeted={targetedUnit} setTargeted={setTargetedUnit}
+                  throwDone={throwDone} setThrowDone={setThrowDone}
+                  succThrows={succThrows} setSuccThrows={setSuccThrows}
+                />}
             </div>
             }
         </div>
